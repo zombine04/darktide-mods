ec2d0c705ce417e346e28aa322723df6aa752def
deluxghost <deluxghost@users.noreply.github.com>
2023-03-28 17:37:46 +0800

    add zh-cn to quickest play

--- quickest_play/scripts/mods/quickest_play/quickest_play_localization.lua
+++ quickest_play/scripts/mods/quickest_play/quickest_play_localization.lua
@@ -1,26 +1,32 @@
 return {
 	mod_description = {
 		en = "Add hotkey to start quickplay.",
-		ja = "クイックプレイを開始するホットキーを追加します。"
+		ja = "クイックプレイを開始するホットキーを追加します。",
+		["zh-cn"] = "添加开始快速游戏的快捷键。",
 	},
 	qp_difficulty = {
 		en = "Difficulty Settings",
 		ja = "難易度設定",
+		["zh-cn"] = "难度设置",
 	},
 	qp_enable_override = {
 		en = "Override current difficulty",
 		ja = "現在の難易度設定を上書きする",
+		["zh-cn"] = "覆盖当前的难度",
 	},
 	qp_danger = {
 		en = "Difficulty",
 		ja = "難易度",
+		["zh-cn"] = "难度",
 	},
 	qp_keybind = {
 		en = "Keybind",
 		ja = "キーバインド",
+		["zh-cn"] = "快捷键",
 	},
 	qp_locked = {
 		en = "You have not met the required level.\nRequirement: ",
-		ja = "必要レベルを満たしていません。\n必要レベル: "
+		ja = "必要レベルを満たしていません。\n必要レベル: ",
+		["zh-cn"] = "你没有达到所需的等级。\n需要等级：",
 	}
 }
