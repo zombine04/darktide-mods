05451f44df81a0ef4309368699531c361e30e606
zombine <evaseries0404@gmail.com>
2023-04-02 08:33:08 +0900

    fix mod name

--- better_armoury_sorting/scripts/mods/better_armoury_sorting/better_armoury_sorting_data.lua
+++ better_armoury_sorting/scripts/mods/better_armoury_sorting/better_armoury_sorting_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("better_armoury_sorting")
 
 return {
-	name = "better_armoury_sorting",
+	name = "Better Armoury Sorting",
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 }
