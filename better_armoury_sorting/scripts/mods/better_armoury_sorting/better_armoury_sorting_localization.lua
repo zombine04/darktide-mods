4864cd5c61724e88d97837a0b0ee7d325b55abeb
deluxghost <deluxghost@users.noreply.github.com>
2023-04-03 11:20:00 +0800

    update zh-cn

--- better_armoury_sorting/scripts/mods/better_armoury_sorting/better_armoury_sorting_localization.lua
+++ better_armoury_sorting/scripts/mods/better_armoury_sorting/better_armoury_sorting_localization.lua
@@ -1,6 +1,7 @@
 return {
 	mod_description = {
 		en = "Group up and sort weapons by name in Brunt's Armoury.",
-		ja = "ブラントの武器庫で各武器を分類化してソートします。"
+		ja = "ブラントの武器庫で各武器を分類化してソートします。",
+		["zh-cn"] = "在布伦特的军备库中分组和排序武器。",
 	},
 }
