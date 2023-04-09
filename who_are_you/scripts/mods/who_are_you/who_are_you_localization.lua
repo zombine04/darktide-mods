9da4e672763ad48a609214cae2a95dfd0f97e4bf
xsSplater <xssplater@gmail.com>
2023-04-09 14:26:41 +0300

    Update who_are_you_localization.lua

--- who_are_you/scripts/mods/who_are_you/who_are_you_localization.lua
+++ who_are_you/scripts/mods/who_are_you/who_are_you_localization.lua
@@ -1,58 +1,73 @@
 return {
 	mod_name = {
 		en = "Who Are You",
+		ru = "Кто ты",
 	},
 	mod_description = {
 		en = "Display players' account name next to their character name.",
 		ja = "キャラクター名の横にプレイヤーのアカウントネームを表示します。",
+		ru = "Who Are You - Отображет имя учётной записи игрока рядом с именем его персонажа.",
 	},
 	display_style = {
 		en = "Display style",
 		ja = "表示スタイル",
+		ru = "Стиль отображения",
 	},
 	character_first = {
 		en = "Character Name (Account Name)",
 		ja = "キャラクター名 (アカウント名)",
+		ru = "Имя персонажа (Учётная запись)",
 	},
 	account_first = {
 		en = "Account Name (Character Name)",
 		ja = "アカウント名 (キャラクター名)",
+		ru = "Учётная запись (Имя персонажа)",
 	},
 	character_only = {
 		en = "Character Name",
 		ja = "キャラクター名",
+		ru = "Имя персонажа",
 	},
 	account_only = {
 		en = "Account Name",
 		ja = "アカウント名",
+		ru = "Учётная запись",
 	},
 	enable_display_self = {
 		en = "Display your own account name",
 		ja = "自身のアカウント名も表示する",
+		ru = "Видимость имени вашей учётной записи",
 	},
 	sub_name_settings = {
 		en = "Sub name settings",
 		ja = "サブネーム設定",
+		ru = "Настройки дополнительной части имени",
 	},
 	enable_custom_size = {
 		en = "Change sub name size",
 		ja = "サブネームの大きさを変更する",
+		ru = "Изменить размер доп. имени",
 	},
 	sub_name_size = {
 		en = "Size",
 		ja = "大きさ",
+		ru = "Размер",
 	},
 	enable_custom_color = {
 		en = "Change sub name color",
 		ja = "サブネームの色を変更する",
+		ru = "Изменить цвет доп. имени",
 	},
 	color_r = {
 		en = "R",
+		ru = "Красный",
 	},
 	color_g = {
 		en = "G",
+		ru = "Зелёный",
 	},
 	color_b = {
 		en = "B",
+		ru = "Синий",
 	},
 }
