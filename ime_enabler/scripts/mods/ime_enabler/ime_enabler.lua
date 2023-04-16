e1c651a500668d64ece740405d958c981bda575f
zombine <evaseries0404@gmail.com>
2023-04-17 00:44:56 +0900

    added stuff_searcher compatible mode.

--- ime_enabler/scripts/mods/ime_enabler/ime_enabler.lua
+++ ime_enabler/scripts/mods/ime_enabler/ime_enabler.lua
@@ -2,23 +2,48 @@
     title: ime_enabler
     author: zombine
     date: 14/04/2023
-    version: 1.0.0
+    version: 1.1.0
 ]]
 
 local mod = get_mod("ime_enabler")
 
-mod:hook_safe("ConstantElementChat", "update", function(self)
-    local input_widget = self._input_field_widget
-
+local enable_ime = function(input_widget)
     if input_widget then
         Window.set_ime_enabled(input_widget.content.is_writing)
     end
+end
+
+mod:hook_safe("ConstantElementChat", "update", function(self)
+    local input_widget = self._input_field_widget
+
+    enable_ime(input_widget)
 end)
 
 mod:hook_safe("CharacterAppearanceView", "update", function(self)
     local input_widget = self._page_widgets and self._page_widgets[1]
 
-    if input_widget then
-        Window.set_ime_enabled(input_widget.content.is_writing)
+    enable_ime(input_widget)
+end)
+
+mod:hook_safe("UIViewHandler", "close_view", function(self, view_name)
+    local stuff_searcher_views = {
+        "CreditsVendorView",
+        "MarksVendorView",
+        "InventoryWeaponsView",
+        "CraftingModifyView",
+    }
+
+    if mod:get("enable_stuff_searcher_compat") then
+        for _, view in ipairs(stuff_searcher_views) do
+            mod:hook_safe(view, "update", function(self)
+                local input_widget = self._widgets_by_name.stuff_searcher_input
+
+                enable_ime(input_widget)
+            end)
+        end
+    else
+        for _, view in ipairs(stuff_searcher_views) do
+            mod:hook_disable(view, "update")
+        end
     end
 end)
--- ime_enabler/scripts/mods/ime_enabler/ime_enabler_localization.lua
+++ ime_enabler/scripts/mods/ime_enabler/ime_enabler_localization.lua
@@ -5,6 +5,10 @@ return {
 	},
 	mod_description = {
 		en = "Enable IME when input elements are focused.",
-		ja = "入力欄にフォーカスした際にIMEを有効化します。"
+		ja = "入力欄にフォーカスした際にIMEを有効化します。",
 	},
+	enable_stuff_searcher_compat = {
+		en = "Stuff Searcher Compatible Mode",
+		ja = "Stuff Searcher互換モード",
+	}
 }
