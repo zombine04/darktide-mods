d230573c793810f7de8ffd98d03afe9645d49eec
zombine <evaseries0404@gmail.com>
2023-04-17 22:31:35 +0900

    fixed a issue related to mod toggle.

--- range_finder/scripts/mods/range_finder/range_finder.lua
+++ range_finder/scripts/mods/range_finder/range_finder.lua
@@ -2,7 +2,7 @@
     title: range_finder
     author: kanateko
     date: 17/04/2023
-    version: 1.1.2
+    version: 1.1.3
 ]]
 local mod = get_mod("range_finder")
 
@@ -28,26 +28,37 @@ mod:hook("UIHud", "init", function(func, self, elements, visibility_groups, para
     return func(self, elements, visibility_groups, params)
 end)
 
-local function recreate_hud()
+local function recreate_hud(is_disabled)
     local ui_manager = Managers.ui
-    if ui_manager then
-        local hud = ui_manager._hud
-        if hud then
-            local player_manager = Managers.player
-            local player = player_manager:local_player(1)
-            local peer_id = player:peer_id()
-            local local_player_id = player:local_player_id()
-            local elements = hud._element_definitions
-            local visibility_groups = hud._visibility_groups
+    local hud = ui_manager and ui_manager._hud
 
-            hud:destroy()
-            ui_manager:create_player_hud(peer_id, local_player_id, elements, visibility_groups)
+    if hud then
+        local player_manager = Managers.player
+        local player = player_manager:local_player(1)
+        local peer_id = player:peer_id()
+        local local_player_id = player:local_player_id()
+        local elements = hud._element_definitions
+        local visibility_groups = hud._visibility_groups
+
+        if is_disabled then
+            for i, element in ipairs(elements) do
+                if element.class_name == classname then
+                    table.remove(elements, i)
+                end
+            end
         end
+
+        hud:destroy()
+        ui_manager:create_player_hud(peer_id, local_player_id, elements, visibility_groups)
     end
 end
 
 mod.on_all_mods_loaded = function()
     recreate_hud()
+end
+
+mod.on_disabled = function ()
+    recreate_hud(true)
 end
 
 mod:hook_safe("UIViewHandler", "close_view", function(self, view_name)
