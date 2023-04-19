26648b55cc036c71d46c37427f3a8f751571761b
zombine <evaseries0404@gmail.com>
2023-04-20 02:37:47 +0900

    1.0.0

---
+++ reveal_blessings/scripts/mods/reveal_blessings/reveal_blessings_localization.lua
@@ -0,0 +1,15 @@
+return {
+	mod_name = {
+		en = "Reveal Blessings",
+	},
+	mod_description = {
+		en = "Reveal all blessings available for the weapon in Re-Bless screen.",
+		ja = "再祝福画面でその武器に付与可能な祝福をすべて表示します。",
+	},
+	enable_debug_mode = {
+		en = "Enable Debug Mode",
+		ja = "デバッグモードを有効にする",
+		["zh-cn"] = "启用调试模式",
+		ru = "Включить режим отладки",
+	},
+}
