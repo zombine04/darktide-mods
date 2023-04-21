f0b29cc906cfe991862a6ff8ab35ddbc382e4f23
xsSplater <xssplater@gmail.com>
2023-04-22 00:11:42 +0300

    Update reveal_blessings_localization.lua

--- reveal_blessings/scripts/mods/reveal_blessings/reveal_blessings_localization.lua
+++ reveal_blessings/scripts/mods/reveal_blessings/reveal_blessings_localization.lua
@@ -1,10 +1,12 @@
 return {
 	mod_name = {
 		en = "Reveal Blessings",
+		ru = "Раскрытие благословений",
 	},
 	mod_description = {
 		en = "Reveal all blessings available for the weapon in Re-Bless screen.",
 		ja = "再祝福画面でその武器に付与可能な祝福をすべて表示します。",
+		ru = "Reveal Blessings - Показывает все благословения, доступные для оружия, на экране повторного благословения.",
 	},
 	enable_debug_mode = {
 		en = "Enable Debug Mode",
