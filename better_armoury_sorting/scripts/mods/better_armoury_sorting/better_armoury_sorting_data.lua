956e52bc9cb4908be3c5036e466ee0ddc8f6ac8b
deluxghost <deluxghost@users.noreply.github.com>
2023-04-21 17:26:23 +0800

    update zh-cn

--- better_armoury_sorting/scripts/mods/better_armoury_sorting/better_armoury_sorting_data.lua
+++ better_armoury_sorting/scripts/mods/better_armoury_sorting/better_armoury_sorting_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("better_armoury_sorting")
 
 return {
-	name = "Better Armoury Sorting",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 }
