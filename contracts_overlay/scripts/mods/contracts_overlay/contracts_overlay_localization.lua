c90d85ca95fafa3a8cd8df41ca6ee83abdef661c
deluxghost <deluxghost@users.noreply.github.com>
2023-04-29 02:56:58 +0800

    Update contracts_overlay_localization.lua

--- contracts_overlay/scripts/mods/contracts_overlay/contracts_overlay_localization.lua
+++ contracts_overlay/scripts/mods/contracts_overlay/contracts_overlay_localization.lua
@@ -1,18 +1,22 @@
 return {
     mod_name = {
         en = "Contracts Overlay",
+        ["zh-cn"] = "战术覆盖显示协议",
     },
     mod_description = {
         en = "Show active weekly contracts in the tactical overlay.",
         ja = "戦術オーバーレイに進行中の週間契約を表示します。",
+        ["zh-cn"] = "在战术覆盖界面显示当前进行中的每周协议。",
     },
     enable_live_update = {
         en = "Enable live updates",
         ja = "リアルタイム更新を有効にする",
+        ["zh-cn"] = "启用动态更新",
     },
     live_update_desc = {
         en = "Updates count for collecting materials and kills during the mission.",
         ja = "ミッション中に回収した素材の数とキル数を更新します。",
+        ["zh-cn"] = "在任务进行时动态更新收集的材料数和击杀数。",
     },
 	enable_debug_mode = {
 		en = "Enable Debug Mode",
