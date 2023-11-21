558a11ea13ae10289bc1258cc72626309ca146ec
xsSplater <xssplater@gmail.com>
2023-11-21 22:30:59 +0300

    RU InspectFromSocial_localization.lua

--- InspectFromSocial/scripts/mods/InspectFromSocial/InspectFromSocial_localization.lua
+++ InspectFromSocial/scripts/mods/InspectFromSocial/InspectFromSocial_localization.lua
@@ -1,9 +1,11 @@
 return {
     mod_name = {
         en = "Inspect from Social",
+        ru = "Осмотр из социального меню",
     },
     mod_description = {
         en = "Adds \"Inspect Operative\" button to the social menu.",
         ja = "ソーシャルメニューに「隊員を調べる」ボタンを追加します。",
+        ru = "Inspect from Social - Добавляет кнопку для осмотра оперативника в социальное меню.",
     },
 }
