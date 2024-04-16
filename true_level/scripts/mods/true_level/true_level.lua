0ff5a7e433b11700cc3d47b5285ec1fbd75ff444
zombine <evaseries0404@gmail.com>
2024-04-16 23:51:12 +0900

    removed line for debug

--- true_level/scripts/mods/true_level/true_level.lua
+++ true_level/scripts/mods/true_level/true_level.lua
@@ -217,7 +217,7 @@ mod:hook_safe("MainMenuView", "_show_character_details", function(self, show, pr
     local character_id = profile.character_id
     local progression = mod._memory.progression
     local progression_data = progression and progression[character_id]
-    mod:dump(widget.style)
+
     if progression_data then
         local title = content.character_archetype_title
         content.character_archetype_title = mod.replace_level_text(title, progression_data, "main_menu")
