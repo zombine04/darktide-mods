60a96d9a5172058fcde4b8249c7864188c5dd9ee
zombine <evaseries0404@gmail.com>
2024-09-26 05:31:54 +0900

    fixed errors after the Unlocked and Loaded update

--- DirectToHadron/scripts/mods/DirectToHadron/DirectToHadron.lua
+++ DirectToHadron/scripts/mods/DirectToHadron/DirectToHadron.lua
@@ -1,29 +1,17 @@
 --[[
     name: DirectToHadron
     author: Zombine
-    date: 31/10/2023
-    version: 1.2.0
+    date: 2024/09/26
+    version: 1.3.0
 ]]
 
 local mod = get_mod("DirectToHadron")
 
 local _init = function()
     mod.item = nil
-    mod.index = nil
+    mod.gear_id = nil
     mod.slot_name = nil
-    mod.element = nil
 end
-
--- ##############################
--- Inventory: Loadout
--- ##############################
-
-mod:hook_safe(CLASS.InventoryView, "update", function(self)
-    if mod.element and not Managers.ui:view_active("inventory_weapons_view") then
-        self:cb_on_grid_entry_pressed(nil, mod.element)
-        mod.element = nil
-    end
-end)
 
 -- ##############################
 -- Inventory: Item List
@@ -66,16 +54,18 @@ mod:hook(CLASS.InventoryWeaponsView, "_setup_input_legend", function(func, self)
 end)
 
 mod:hook_safe(CLASS.InventoryWeaponsView, "_cb_on_present", function(self)
-    if mod.index then
+    if mod.gear_id then
+        local index = 1
         local grid_widgets = self:grid_widgets()
-        local num_widget = #grid_widgets
 
-        if mod.index > num_widget then
-            mod.index = 1
+        for i, widget in ipairs(grid_widgets) do
+            if widget.content.element.item.gear_id == mod.gear_id then
+                index = i
+            end
         end
 
-        self:focus_grid_index(mod.index, 0 , true)
-        self:scroll_to_grid_index(mod.index, true)
+        self:focus_grid_index(index, 0 , true)
+        self:scroll_to_grid_index(index, true)
         _init()
     end
 end)
@@ -85,18 +75,25 @@ end)
 -- ##############################
 
 mod:hook_safe(CLASS.CraftingView, "on_enter", function(self)
-    if mod:get("enable_skip_hadron") or mod.item then
+    local um = Managers.ui
+    local weapons_view = "inventory_weapons_view"
+
+    if um:view_active(weapons_view) then
+        um:close_view(weapons_view)
+    end
+
+    if mod:get("enable_skip_hadron") and mod.item then
         self:on_option_button_pressed(nil, {
             callback = function (crafting_view)
-                crafting_view:go_to_crafting_view("select_item", mod.item)
+                crafting_view:go_to_crafting_view("select_item_mechanicus", mod.item)
             end
         })
     end
 end)
 
 mod:hook_safe(CLASS.CraftingView, "update", function(self)
-    if not self.dth_closed and self._active_view == nil and self._previously_active_view_name == "crafting_modify_view" then
-        if mod:get("enable_skip_hadron") or mod.item then
+    if not self.dth_closed and self._active_view == nil and self._previously_active_view_name == "crafting_mechanicus_modify_view" then
+        if mod:get("enable_skip_hadron") and mod.item then
             self.dth_closed = true
             Managers.ui:close_view("crafting_view")
         end
@@ -104,41 +101,16 @@ mod:hook_safe(CLASS.CraftingView, "update", function(self)
 end)
 
 mod:hook_safe(CLASS.CraftingView, "on_exit", function(self)
-    local um = Managers.ui
-    local weapons_view = "inventory_weapons_view"
-    local inventory_view = "inventory_view"
+    local inventory = Managers.ui:view_instance("inventory_view")
+    local widgets = inventory and inventory._loadout_widgets
 
-    if um:view_active(weapons_view) then
-        local view = um:view_instance(weapons_view)
-        local selected_slot = view._selected_slot
-        local curio_slot = "slot_attachment_"
+    if widgets then
+        for _, widget in ipairs(widgets) do
+            local element = widget.content.element
 
-        if not selected_slot then
-            return
-        end
-
-        -- ignore curio slot number
-        if string.match(selected_slot.name, curio_slot) and string.match(mod.slot_name, curio_slot) then
-            mod.slot_name = selected_slot.name
-        end
-
-        if selected_slot.name == mod.slot_name then
-            view:_fetch_inventory_items(selected_slot)
-        else
-            um:close_view(weapons_view)
-            view = um:view_instance(inventory_view)
-
-            local widgets = view._loadout_widgets
-
-            if widgets then
-                for i, widget in ipairs(widgets) do
-                    local element = widget.content.element
-
-                    if element.slot and element.slot.name == mod.slot_name then
-                        mod.element = element
-                        break
-                    end
-                end
+            if element and element.slot and element.slot.name == mod.slot_name then
+                inventory:cb_on_grid_entry_pressed(nil, element)
+                break
             end
         end
     end
@@ -148,7 +120,9 @@ end)
 -- Crafting: Item List
 -- ##############################
 
-mod:hook_safe(CLASS.CraftingModifyView, "_preview_item", function(self, item)
-    mod.index = self:selected_grid_index()
+mod:hook_safe(CLASS.CraftingMechanicusModifyView, "_preview_item", function(self, item)
+    local widget = self:selected_grid_widget()
+
+    mod.gear_id = widget.content.element.item.gear_id
     mod.slot_name = self:_fetch_item_compare_slot_name(item)
 end)
