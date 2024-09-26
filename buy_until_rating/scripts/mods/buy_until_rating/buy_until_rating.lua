f4b2df7b8ad35ddd02737cda5fb662bb4ae6be18
zombine <evaseries0404@gmail.com>
2024-09-26 19:53:32 +0900

    updated discarding method

--- buy_until_rating/scripts/mods/buy_until_rating/buy_until_rating.lua
+++ buy_until_rating/scripts/mods/buy_until_rating/buy_until_rating.lua
@@ -2,7 +2,7 @@
     title: buy_until_rating
     author: Zombine
     date: 2024/09/26
-    version: 2.2.5
+    version: 2.3.0
 ]]
 
 local mod = get_mod("buy_until_rating")
@@ -16,12 +16,10 @@ mod._highest = 0
 mod._desired_rating = mod:get("desired_rating")
 mod._num_limit = mod:get("num_limit")
 mod._discard_threshold = mod:get("discard_threshold")
+mod._delete_promise = nil
+mod._items_to_discard = {}
 
-mod._ready_to_discard = false
-mod._discard_garbage = false
-mod._garbages = {}
-
-local init = function()
+local _init = function()
      mod._canceled = false
      mod._acquired_items = {}
      mod._highest = 0
@@ -30,20 +28,20 @@ local init = function()
      mod._discard_threshold = mod:get("discard_threshold")
 end
 
-local is_enabled = function(key)
+local _is_enabled = function(key)
     return mod:get("enable_" .. key)
 end
 
-local get_current_settings = function()
+local _get_current_settings = function()
     local current_settings = mod:localize("desired_rating") .. ": " ..  mod._desired_rating
 
-    if is_enabled("num_limit") then
+    if _is_enabled("num_limit") then
         current_settings = current_settings .. "\n" .. mod:localize("num_limit") .. ": " ..  mod._num_limit
     else
         current_settings = current_settings .. "\n" .. mod:localize("num_limit") .. ": " .. mod:localize("unlimited")
     end
 
-    if is_enabled("auto_discard") then
+    if _is_enabled("auto_discard") then
         current_settings = current_settings .. "\n" .. mod:localize("auto_discard") .. ": " ..  mod._discard_threshold .. ' ' .. mod:localize("lower")
     else
         current_settings = current_settings .. "\n" .. mod:localize("auto_discard") .. ": " .. mod:localize("disabled")
@@ -52,14 +50,14 @@ local get_current_settings = function()
     return current_settings
 end
 
-local clear_notifications = function(force_clear)
+local _clear_notifications = function(force_clear)
     if force_clear or #mod._acquired_items % 5 == 0 then
         Managers.event:trigger("event_clear_notifications")
     end
 end
 
-local is_less_than_limit = function()
-    if is_enabled("num_limit") then
+local _is_less_than_limit = function()
+    if _is_enabled("num_limit") then
         return #mod._acquired_items <  mod._num_limit
     end
 
@@ -102,50 +100,57 @@ local _get_character_save_data = function ()
     return character_data
 end
 
-local discard_garbage = function()
-    local gear_id = mod._garbages[1]
-    local next_gear = function()
-        Managers.event:trigger("event_vendor_view_purchased_item")
-        table.remove(mod._garbages, 1)
+local _discard_items = function()
+    local delete_promise = Managers.data_service.gear:delete_gear_batch(mod._items_to_discard)
 
-        if not table.is_empty(mod._garbages) then
-            mod._ready_to_discard = true
+    mod._delete_promise = delete_promise
+
+    delete_promise:next(function(result)
+        local total_rewards = {}
+
+        if result then
+            for i = 1, #result do
+                local operation = result[i]
+                local rewards = operation.rewards
+
+                for i = 1, #rewards do
+                    local reward = rewards[i]
+                    local reward_type = reward.type
+
+                    total_rewards[reward_type] = (total_rewards[reward_type] or 0) + reward.amount
+                end
+            end
         end
-    end
 
-    Managers.data_service.gear:delete_gear(gear_id):next(function(result)
-        local rewards = result and result.rewards
+        if not table.is_empty(total_rewards) then
+            Managers.event:trigger("event_vendor_view_purchased_item")
 
-        if rewards then
-            local credits_amount = rewards[1] and rewards[1].amount or 0
-
-            if is_enabled("discard_notif") then
+            for reward_type, reward_amount in pairs(total_rewards) do
                 Managers.event:trigger("event_add_notification_message", "currency", {
-                    currency = "credits",
-                    amount = credits_amount
+                    currency = reward_type,
+                    amount = reward_amount
                 })
             end
-
-            next_gear()
         end
+
+        mod._delete_promise = nil
     end):catch(function(e)
-        mod:dump(e, "delete_gear", 3)
-        next_gear()
+        mod._delete_promise = nil
     end)
 end
 
-local make_garbage_list = function()
+local _make_garbage_list = function()
     local items = mod._acquired_items
-    local garbages = mod._garbages
+    local garbages = mod._items_to_discard
 
-    for i, item in ipairs(items) do
-        if item.is_garbage then
-            garbages[#garbages + 1] = items[i].gear_id
+    for i, item_data in ipairs(items) do
+        if item_data.is_garbage then
+            garbages[#garbages + 1] = items[i].item
         end
     end
 end
 
-local mark_acquired_items_as_new = function()
+local _mark_acquired_items_as_new = function()
     local character_data = _get_character_save_data()
 
     if not character_data then
@@ -157,7 +162,7 @@ local mark_acquired_items_as_new = function()
     end
 
     for _, item in ipairs(mod._acquired_items) do
-        if not (is_enabled("auto_discard") and item.is_garbage) then
+        if not (_is_enabled("auto_discard") and item.is_garbage) then
             local gear_id = item.gear_id
             local item_type = item.item_type
             local new_items = character_data.new_items
@@ -183,12 +188,12 @@ local mark_acquired_items_as_new = function()
     Managers.event:trigger("event_resync_character_news_feed")
 end
 
-local show_results = function()
+local _show_results = function()
     local results = mod:localize("num") .. ": " .. #mod._acquired_items
     local max = mod:localize("max") .. ": " ..  mod._highest
     local avg = mod:localize("avg") .. ": " .. _calc_average()
 
-    if is_enabled("auto_discard") then
+    if _is_enabled("auto_discard") then
         local discarded = mod:localize("discarded") .. ": " .. _get_discarded_count()
         results = results .. "\n" .. discarded
     end
@@ -198,13 +203,13 @@ local show_results = function()
     Managers.ui:play_2d_sound("wwise/events/ui/play_hud_notifications_item_tier_3")
     mod:notify(results)
 
-    if is_enabled("print_result") then
+    if _is_enabled("print_result") then
         mod:echo("\n" .. results)
     end
 end
 
 local prevent_close_view = function(func, ...)
-    if not table.is_empty(mod._garbages) then
+    if mod._delete_promise then
         return
     end
 
@@ -214,7 +219,7 @@ end
 local prevent_inputs = function(func, ...)
     local out = func(...)
 
-    if not table.is_empty(mod._garbages) and type(out) == "boolean" then
+    if mod._delete_promise and type(out) == "boolean" then
         return false
     end
 
@@ -236,24 +241,12 @@ mod:hook("InputService", "_get", prevent_inputs)
 mod:hook("InputService", "_get_simulate", prevent_inputs)
 
 mod:hook_safe("CreditsGoodsVendorView", "init", function()
-    init()
-    clear_notifications(true)
-end)
-
-mod:hook_safe("CreditsGoodsVendorView", "update", function(self)
-    if mod._discard_garbage then
-        mod._discard_garbage = false
-        discard_garbage()
-    end
-
-    if mod._ready_to_discard and self._wallet_promise == nil then
-        mod._ready_to_discard = false
-        mod._discard_garbage = true
-    end
+    _init()
+    _clear_notifications(true)
 end)
 
 mod:hook_safe("CreditsGoodsVendorView", "cb_switch_tab", function()
-    init()
+    _init()
 end)
 
 mod:hook_safe("CreditsGoodsVendorView", "_preview_element", function(self)
@@ -265,10 +258,10 @@ mod:hook_safe("CreditsGoodsVendorView", "_preview_element", function(self)
     local price = offer.price.amount.amount or 0
     local price_total = ""
     local color = "{#color(215, 215, 160)}"
-    local current_settings = get_current_settings()
+    local current_settings = _get_current_settings()
 
     if price ~= 0 then
-        if is_enabled("num_limit") then
+        if _is_enabled("num_limit") then
             price_total = " (" .. price *  mod._num_limit .. ")"
         else
             price_total = " (" .. mod:localize("unlimited") .. ")"
@@ -289,7 +282,7 @@ mod:hook_safe("CreditsGoodsVendorView", "_on_purchase_complete", function(self, 
     end
 
     Managers.event:trigger("event_vendor_view_purchased_item")
-    clear_notifications()
+    _clear_notifications()
 
     for _, item_data in ipairs(items) do
         local uuid = item_data.uuid
@@ -300,10 +293,11 @@ mod:hook_safe("CreditsGoodsVendorView", "_on_purchase_complete", function(self, 
             local index = #mod._acquired_items + 1
 
              mod._acquired_items[index] = {
+                item = item,
                 gear_id = item.gear_id,
                 item_type = item.item_type,
                 rating = rating,
-                is_garbage = rating <=  mod._discard_threshold,
+                is_garbage = rating <= mod._discard_threshold,
             }
 
             if rating >  mod._highest then
@@ -314,26 +308,23 @@ mod:hook_safe("CreditsGoodsVendorView", "_on_purchase_complete", function(self, 
         end
     end
 
-    if  mod._highest <  mod._desired_rating and is_less_than_limit() and not  mod._canceled then
+    if  mod._highest < mod._desired_rating and _is_less_than_limit() and not mod._canceled then
         self:_update_button_disable_state()
         self:_cb_on_purchase_pressed()
     else
-        clear_notifications(true)
+        _clear_notifications(true)
 
         if  mod._canceled then
             mod:notify(mod:localize("canceled"))
         end
 
-        if is_enabled("auto_discard") then
-            make_garbage_list()
-
-            if not table.is_empty(mod._garbages) then
-                mod._discard_garbage = true
-            end
+        if _is_enabled("auto_discard") then
+            _make_garbage_list()
+            _discard_items()
         end
-        mark_acquired_items_as_new()
-        show_results()
-        init()
+        _mark_acquired_items_as_new()
+        _show_results()
+        _init()
     end
 end)
 
