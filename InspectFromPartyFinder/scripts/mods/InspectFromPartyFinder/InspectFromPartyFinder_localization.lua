7d5cab8df6ed1fe0a8af5040f34b7ec480202d3c
deluxghost <deluxghost@users.noreply.github.com>
2024-12-16 14:58:00 +0800

    Update InspectFromPartyFinder_localization.lua

--- InspectFromPartyFinder/scripts/mods/InspectFromPartyFinder/InspectFromPartyFinder_localization.lua
+++ InspectFromPartyFinder/scripts/mods/InspectFromPartyFinder/InspectFromPartyFinder_localization.lua
@@ -1,9 +1,11 @@
 return {
     mod_name = {
         en = "Inspect from Party Finder",
+        ["zh-cn"] = "寻找队伍界面查看装备",
     },
     mod_description = {
         en = "Adds a function to inspect players in the party finder.",
         ja = "パーティーファインダーにプレイヤーを調べる機能を追加します。",
+        ["zh-cn"] = "允许在寻找队伍界面查看玩家装备",
     },
 }
