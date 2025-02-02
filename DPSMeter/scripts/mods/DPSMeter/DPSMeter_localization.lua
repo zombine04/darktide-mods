cfe306c5ef3cbc1c73b472a101981e349512c06b
deluxghost <deluxghost@users.noreply.github.com>
2025-02-02 23:32:40 +0800

    Update DPSMeter_localization.lua

--- DPSMeter/scripts/mods/DPSMeter/DPSMeter_localization.lua
+++ DPSMeter/scripts/mods/DPSMeter/DPSMeter_localization.lua
@@ -1,32 +1,40 @@
 return {
     mod_name = {
         en = "DPS Meter",
+        ["zh-cn"] = "DPS 指示器",
     },
     mod_description = {
         en = "Displays DPS.",
         ja = "DPSを表示します。",
+        ["zh-cn"] = "显示 DPS（每秒造成伤害）。",
     },
     calc_method = {
         en = "Calculation Method",
         ja = "計算方法",
+        ["zh-cn"] = "计算方式",
     },
     calc_method_tooltip = {
         en = "Average: Displays average damage per second and resets if no damage is dealt for 5 seconds.\n\n" ..
              "Sum: Displays the total damage dealt within 1 second.",
         ja = "平均：1秒あたりの平均ダメージを表示し、最後の攻撃から5秒経過するとリセットされます。\n\n"..
              "加算：1秒間で与えたダメージの合計を表示します。",
+        ["zh-cn"] = "平均：显示每秒的平均伤害，如果 5 秒未造成伤害则重置。\n\n" ..
+             "总和：显示 1 秒内造成的总伤害。",
     },
     calc_method_average = {
         en = "Average",
         ja = "平均",
+        ["zh-cn"] = "平均",
     },
     calc_method_sum = {
         en = "Sum",
         ja = "加算",
+        ["zh-cn"] = "总和",
     },
     shooting_range_only = {
         en = "Display only within the Psykhanium",
         ja = "サイカニウムにいる時のみ表示する",
+        ["zh-cn"] = "仅在灵能室内显示",
     },
     group_keybinds = {
         en = "Keybinds",
@@ -37,10 +45,12 @@ return {
     group_font_settings = {
         en = "Font Settings",
         ja = "フォント設定",
+        ["zh-cn"] = "字体设置",
     },
     hotkey_reset_meter = {
         en = "Reset Meter",
         ja = "メーターのリセット",
+        ["zh-cn"] = "重置指示器",
     },
     font_size = {
         en = "Size",
